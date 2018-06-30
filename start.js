87784ea268426ef1ae62acd8106f84ba78cb1c3f
Jan Maršíček <janmarsicek@janmarsicek.cz>
2018-06-30 15:55:44 +0200

    Connect to the db

--- start.js
+++ start.js
@@ -1,5 +1,21 @@
-const app = require("./app");
+require('dotenv').config();
 
+/* database */
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err}`);
+  });
+
+/* express app */
+const app = require("./app");
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
 });
